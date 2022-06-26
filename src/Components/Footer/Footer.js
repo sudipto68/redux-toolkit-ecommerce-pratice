1eb8aa4e93d4fe1e62860d9b5475529bb355b45c
sudipto68 <sudiptomitro2016@gmail.com>
2022-06-26 18:53:48 +0600

    small changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,11 @@ import styles from "./footer.module.scss";
 
 const Footer = () => {
   const year = new Date().getFullYear();
-  return <div className={styles.footer}>Copyright © {year} - Sudipto LLC</div>;
+  return (
+    <div className={styles.footer}>
+      Copyright © {year} - Redux Commerce INC.
+    </div>
+  );
 };
 
 export default Footer;
--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { STATUS } from "../../constants/Status";
@@ -8,13 +8,22 @@ import styles from "./productlist.module.scss";
 import Loader from "../Loader/Loader";
 
 const ProductList = () => {
+  const [limitProducts, setLimitProducts] = useState(true);
+
+  const categories = [
+    "electronics",
+    "jewelery",
+    "men's clothing",
+    "women's clothing",
+  ];
   const dispatch = useDispatch();
 
   const { products, status } = useSelector((state) => state.products);
 
+  const productList = limitProducts ? products.slice(0, 6) : products;
+
   useEffect(() => {
     dispatch(fetchProduct());
-    console.log(status);
   }, []);
 
   if (status === STATUS.LOADING) {
@@ -30,9 +39,17 @@ const ProductList = () => {
       <Container>
         <h2>Our Products</h2>
         <div className={styles.productList}>
-          {products?.map((product) => {
+          {productList?.map((product) => {
             return <ProductCard key={product.id} product={product} />;
           })}
+        </div>
+        <div className="text-center py-5">
+          <button
+            className="btn loadBtn text-center"
+            onClick={() => setLimitProducts(!limitProducts)}
+          >
+            {limitProducts ? "Load More Products" : "Load Limited Products"}
+          </button>
         </div>
       </Container>
     </div>
